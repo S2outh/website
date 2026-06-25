fa393dbe1e42186e953a559f19475d5560bbc820
eleocraft <eliseo.cailloux@gmx.de>
2026-06-25 14:58:44 +0200

    error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<div class="content-div">
+  <h1>{page.status}</h1>
+  <h2>{page.error!.message}</h2>
+</div>
+
+<style>
+  h1 {
+    font-size: 500%;
+  }
+  .content-div {
+    text-align: center;
+  }
+</style>
